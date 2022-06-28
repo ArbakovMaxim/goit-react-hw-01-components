5a82da7cc0ca5c4925970ddb31b46b089b20d96c
Arbakov Maxim <fefaodessa@gmail.com>
2022-06-29 00:26:39 +0300

    -app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Profile } from 'Feature/Profile/Profile';
-import { Main } from 'App.styled';
+import { Main } from './components/Ui/Main.styled';
 import { Statistics } from 'Feature/Statistics/Statistics';
 import { FriendsList } from 'Feature/FriendList/FriendList/FriendLists';
 import { TransactionHistory } from 'Feature/TransactionHistory/TransactionHistory';
